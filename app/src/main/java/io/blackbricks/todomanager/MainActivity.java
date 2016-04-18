ff4cf4dfda6b8f3fe5476e09dc38c6afee13c30f
Kryndach <kryndach.ios@gmail.com>
2016-04-18 19:10:58 +0600

    start use main activity

--- app/src/main/java/io/blackbricks/todomanager/MainActivity.java
+++ app/src/main/java/io/blackbricks/todomanager/MainActivity.java
@@ -1,14 +1,45 @@
 package io.blackbricks.todomanager;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+
+import javax.inject.Inject;
+
+import butterknife.Bind;
+import icepick.Icicle;
+import io.blackbricks.todomanager.menu.MenuFragment;
+import io.blackbricks.todomanager.taskList.TaskListActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Icicle
+    String toolbarTitle;
+
+    @Inject
+    IntentStarter intentStarter;
+
+    @Bind(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.contentView)
+    ViewGroup contentView;
+
+    ActionBarDrawerToggle drawerToggle;
+    private MainActivityComponent mainActivityComponent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contentView, new Builderdddd, null)
+                .commit();
     }
 }
--- app/src/main/java/io/blackbricks/todomanager/taskList/TaskListActivity.java
+++ app/src/main/java/io/blackbricks/todomanager/taskList/TaskListActivity.java
@@ -1,9 +1,13 @@
 package io.blackbricks.todomanager.taskList;
 
+import android.app.Activity;
+
+import io.blackbricks.todomanager.base.view.BaseActivity;
+
 /**
  * Created by yegorkryndach on 14/04/16.
  */
-public class TaskListActivity {
+public class TaskListActivity extends BaseActivity {
 
     public static final String KEY_FILTER =
             "io.blackbricks.todomanager.taskList.TaskListActivity.FILTER";

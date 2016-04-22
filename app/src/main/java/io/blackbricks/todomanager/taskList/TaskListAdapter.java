4f59d03ed22bbd0b4b15c7693b10dce83fa24765
Kryndach <kryndach.ios@gmail.com>
2016-04-22 17:50:59 +0600

    task list item resource

--- app/src/main/java/io/blackbricks/todomanager/taskList/TaskListAdapter.java
+++ app/src/main/java/io/blackbricks/todomanager/taskList/TaskListAdapter.java
@@ -1,17 +1,33 @@
 package io.blackbricks.todomanager.taskList;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hannesdorfmann.annotatedadapter.annotation.ViewField;
+import com.hannesdorfmann.annotatedadapter.annotation.ViewType;
 import com.hannesdorfmann.annotatedadapter.support.recyclerview.SupportAnnotatedAdapter;
 
 import java.util.ArrayList;
 
+import io.blackbricks.todomanager.R;
 import io.blackbricks.todomanager.model.Task;
 
 /**
  * Created by yegorkryndach on 22/04/16.
  */
-public class TaskListAdapter extends SupportAnnotatedAdapter {
+public class TaskListAdapter extends SupportAnnotatedAdapter implements TaskListAdapterBinder {
+
+    @ViewType(layout = R.layout.list_task_item,
+            initMethod = true,
+            views = {
+                    @ViewField(id = R.id.icon, name = "icon", type = ImageView.class),
+                    @ViewField(id = R.id.title, name = "title", type = TextView.class),
+                    @ViewField(id = R.id.title, name = "description", type = TextView.class),
+            })
+    public final int taskItem = 0;
 
     ArrayList<Task> taskList;
 
@@ -30,5 +46,17 @@ public class TaskListAdapter extends SupportAnnotatedAdapter {
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    @Override
+    public void initViewHolder(TaskListAdapterHolders.TaskItemViewHolder vh, View view, ViewGroup parent) {
+
+    }
+
+    @Override
+    public void bindViewHolder(TaskListAdapterHolders.TaskItemViewHolder vh, int position) {
+        final Task task = taskList.get(position);
+        vh.icon.setImageResource(task.getIconId());
+        vh.title.setText(task.getTitle());
     }
 }

b428a4efdf28f4f09dd234039ea1db11f918260d
Kryndach <kryndach.ios@gmail.com>
2016-06-09 12:11:22 +0600

    created Auth Classes

---
+++ app/src/main/java/io/blackbricks/todomanager/auth/login/LoginFragment.java
@@ -0,0 +1,7 @@
+package io.blackbricks.todomanager.auth.login;
+
+/**
+ * Created by yegorkryndach on 09/06/16.
+ */
+public class LoginFragment {
+}
---
+++ app/src/main/java/io/blackbricks/todomanager/auth/registration/RegistrationActivity.java
@@ -0,0 +1,7 @@
+package io.blackbricks.todomanager.auth.registration;
+
+/**
+ * Created by yegorkryndach on 09/06/16.
+ */
+public class RegistrationActivity {
+}

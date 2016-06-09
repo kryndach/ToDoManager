1a1d39f80c0c02b74143ab1cb7ce66cc68b11acb
Kryndach <kryndach.ios@gmail.com>
2016-06-09 15:05:52 +0600

    authService complete

--- app/src/main/java/io/blackbricks/todomanager/api/AuthService.java
+++ app/src/main/java/io/blackbricks/todomanager/api/AuthService.java
@@ -1,13 +1,25 @@
 package io.blackbricks.todomanager.api;
 
-import retrofit2.http.GET;
-import retrofit2.http.Headers;
-import retrofit2.http.Path;
+import okhttp3.ResponseBody;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by yegorkryndach on 09/06/16.
  */
 public interface AuthService {
 
+    @POST("api/v1/users/create")
+    Observable<ResponseBody> create(
+            @Query("username") String username,
+            @Query("email") String email,
+            @Query("password") String password
+    );
 
+    @POST("api/v1/login")
+    Observable<ResponseBody> login(
+            @Query("username") String username,
+            @Query("password") String password
+    );
 }

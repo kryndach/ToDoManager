47e3fe0c91dc91f0418ad958e3c238da98299d5a
Kryndach <kryndach.ios@gmail.com>
2016-06-14 18:51:20 +0600

    cleanup login activity

--- app/src/main/java/io/blackbricks/todomanager/auth/login/LoginActivity.java
+++ app/src/main/java/io/blackbricks/todomanager/auth/login/LoginActivity.java
@@ -1,27 +1,21 @@
 package io.blackbricks.todomanager.auth.login;
 
-import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 
 import javax.inject.Inject;
 
 import io.blackbricks.todomanager.R;
 import io.blackbricks.todomanager.ToDoManagerApp;
-import io.blackbricks.todomanager.api.response.LoginResponse;
-import io.blackbricks.todomanager.api.service.AuthService;
 import io.blackbricks.todomanager.base.view.BaseActivity;
 import io.blackbricks.todomanager.databinding.ActivityLoginBinding;
-import rx.functions.Action1;
 
 /**
  * Created by yegorkryndach on 09/06/16.
  */
 public class LoginActivity extends BaseActivity {
 
-    private LoginComponent mLoginComponent;
     @Inject
     LoginPresenter mLoginPresenter;
 
@@ -35,10 +29,10 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void injectDependencies() {
-        mLoginComponent = DaggerLoginComponent.builder()
+        LoginComponent loginComponent = DaggerLoginComponent.builder()
                 .appComponent(ToDoManagerApp.getAppComponent())
                 .build();
-        mLoginComponent.inject(this);
+        loginComponent.inject(this);
     }
 
     public void onClickLogin(View view) {
